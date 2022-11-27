c936bf041e2689071eccab4188c1bf0bc6dcbac4
binnasser <abdulmajeednasser@gmail.com>
2022-11-28 00:16:59 +0300

    update city

--- app/Http/Resources/TenantContactResource.php
+++ app/Http/Resources/TenantContactResource.php
@@ -21,11 +21,11 @@ class TenantContactResource extends JsonResource
             'mobile_number' => $this->contact->mobile_number,
             'is_property_buyer' => $this->is_property_buyer,
             'is_property_owner' => $this->is_property_owner,
-            'district' => [
-                'id' => !$this->district ?: $this->district->id,
-                'name_ar' => !$this->district ?: $this->district->name_ar,
-                'name_en' => !$this->district ?: $this->district->name_en,
-            ],
+            'city' => $this->city ? [
+                'id' => $this->city->id,
+                'name_ar' => $this->city->name_ar,
+                'name_en' => $this->city->name_en,
+            ] : null,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
